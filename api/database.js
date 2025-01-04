1eed99a8c0cf3daeeaba1be81378cf3abf48aa39
Luis Frentzen Salim <54829425+luisfrentzen@users.noreply.github.com>
2025-01-04 21:46:08 +0800

    removed comments and debugging

--- api/database.js
+++ api/database.js
@@ -16,41 +16,15 @@ export default async (req, res) => {
 
     const processedData = processData(data.results);
     res.json(processedData);
-    
-    // try {
-    //     const response = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Authorization': `Bearer "${token}"`,
-    //             'Notion-Version': '2021-05-13',
-    //             'Content-Type': 'application/json'
-    //         },
-    //     });
-    //     const data = await response.json();
-
-    //     if (!response.ok) {
-    //         throw new Error(`Notion API error: ${response.status} ${JSON.stringify(data)}`);
-    //     }
-
-    //     const processedData = processData(data.results);
-    //     res.json(processedData);
-    // } catch (error) {
-    //     console.error("Error processing request:", error);
-    //     res.status(500).json({ error: error.message });
-    // }
 };
 
 const processData = (data) => {
     const progressMap = new Map();
 
     data.forEach(item => {
-        console.log(item)
         if (item.properties.Date && item.properties.Progress) {
             if (item.properties.Progress.number !== null && item.properties.Progress.number > 0) {
                 const dateObject = new Date(item.properties.Date.date.start);
-                console.log(dateObject)
-                dateObject.setDate(dateObject.getDate() + 1); // Add one day to the date
-                console.log(dateObject)
                 const date = dateObject.toISOString().split('T')[0]; // Format back to YYYY-MM-DD
                 const progress = Math.round(item.properties.Progress.number * 100); // Convert from 0-1 to 0-100
                 progressMap.set(date, progress);
